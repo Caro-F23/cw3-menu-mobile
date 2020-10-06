bbfb42822ff1b51ce2c055e63f8dc00907ce3726
Caroline Forget <caro.forget23@gmail.com>
2020-10-05 21:20:34 -0400

    Exercice terminee. Menu hamburger adapte au mobile.

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-    //Un commentaire
-    console.log("Ça fonctionne!!!");
+    var hamburger = document.querySelectorAll(".hamburger");
+    //
+    var menuMobile = document.querySelector(".nav-primary-mobile");
+
+    for(var i = 0; i < hamburger.length; i++) {
+        hamburger[i].addEventListener("click", openMenu);
+    }
+
+
+
+    function openMenu(evt) {
+        evt.preventDefault();
+
+        var cible = evt.currentTarget;
+
+        if (cible.classList.contains("open")){
+            cible.classList.remove("open");
+            menuMobile.classList.remove("open");
+        } else {
+            cible.classList.add("open");
+            menuMobile.classList.add("open");
+        }
+    }
 
 });
